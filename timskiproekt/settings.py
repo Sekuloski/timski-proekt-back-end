9cefa1c2e53860e65a40a20b1e1c46dca21fbbc8
Bojan Sekuloski <bojan@sekuloski.mk>
2023-08-07 11:48:29 +0000

    Making fixes to host on server

--- timskiproekt/settings.py
+++ timskiproekt/settings.py
@@ -13,6 +13,7 @@ from datetime import timedelta
 from pathlib import Path
 
 from django.conf import settings
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -165,8 +166,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+CSRF_TRUSTED_ORIGINS = ['http://sekuloski.mk', 'http://sekuloski.mk:27015']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
